c5d7f862b1b6a8e9b9b81d26553381fd92175159
isaactiagowandermurem <141565649+isaactiago@users.noreply.github.com>
2024-03-31 10:38:00 -0300

    crerat new folders

---
+++ manipulacoes/script.js
@@ -0,0 +1,78 @@
+// aula 1 -- Métodos de String (1/3)
+
+//let nome = 'isaac tiago wandermuerem '
+
+
+//let res  = ' ' ;
+
+//if(nome.indexOf('isaac') > -1 ){
+    // console.log("Tem sim ")
+//}else {
+  //  console.log('nao tem ')
+//}
+
+
+//let resultado = nome.indexOf('tiago')// vai retorna pra mim a posição que o texto q a gente digitar/esse texto começa. e se ele nao acha retorna -1 .
+
+//console.log(resultado)
+
+
+//nome.length 
+
+
+//console.log(resultado)
+
+
+//console.log(nome.length) //  vai deizer qunatos caracteres vai ter na variavel nome 
+
+
+
+
+//aula 2 --  Métodos de String (2/3)
+
+//let nome = 'isaac tiagok wansdermeurm '
+
+
+
+
+//let res = nome.substring() //igual o slice.So q ele nao funciona de tras para frente 
+
+//let res = nome.slice(10,20)  // ele vai pega o pedaço da string da onde vc quer que comesse ate aonde voce quer que termine . SE voce bota um numero negativo ele começa do ultimo 
+
+//console.log(res)
+
+
+// aula 3  -- Métodos de String (3/3) 
+
+//let nome = 'isaac gdfgdfg gfdgdf' ; 
+
+//let numero = '1,2,3,4,5,6,7,8,9,10'
+
+//let res = numero.split(',')
+
+
+//let res = nome.split(' ') //transforma string em array 
+
+//let res = nome.charAt(3) ; //qual caractere que vai tar na posição que vc quiser 
+//let res  = nome.trim() //vai tirar os espaços vazios de um string 
+
+
+//let res = nome.toUpperCase() // vai transforma tudo em maiusculo 
+
+//toLoweCase = tudo minusculo 
+
+//let res = nome.replace('tiago','souza')//vai troca uma coisa que esta, por outra que eu digita  ; 
+
+//console.log(res)
+
+
+// aula 4 -- Métodos de Numbers 
+
+
+let n = 10 ; 
+
+let res = n.toString ; // vai trasforma um numero em string 
+
+console.log(res)
+
+
