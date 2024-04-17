c0e66b3e86a046637305643759a8c85c90798590
isaactiago <141565649+isaactiago@users.noreply.github.com>
2024-04-17 19:25:05 -0300

    new folders

---
+++ heheh/questions.js
@@ -0,0 +1,66 @@
+let questions = [
+
+    {
+        question : 'Qual é o livro da Bíblia que descreve a criação do mundo e a história de Adão e Eva?' , 
+
+        option : [
+            'Gênesis' , 
+            'Êxodo' ,
+            'Levítico' ,
+            'Deuteronômio' ,
+        ] ,
+
+        answer : 0 ,
+    } ,
+
+    {
+        question:'Quem foi o rei que construiu o Templo de Jerusalém de acordo com a Bíblia?', 
+
+        option: [
+            'Davi',
+            'salomão' ,
+            'Josias' ,
+            'Ezequias' ,
+        ] ,
+        answer : 1, 
+    } ,
+
+    {
+
+        question: 'Quantos livros compõem o Novo Testamento da Bíblia?' ,
+
+        option : [
+            24 ,
+            27,
+            39,
+            66 ,
+        ], answer: 1 , 
+    } , 
+
+
+    {
+
+        question : 'Qual dos seguintes não é um dos evangelhos do Novo Testamento?' ,
+
+        option : [
+            'Matheus' ,
+            'Marcos' ,
+            'Lucas',
+            'Judas',
+        ] , 
+        answer : 3 ,
+    } ,
+
+    {
+        question : 'Quem liderou o Êxodo do povo de Israel do Egito para a Terra Prometida?' ,
+
+        option: [
+            'Moisés' ,
+            'Arâo' ,
+            'Josué', 
+            'Elias',
+        ] ,
+        answer: 0 ,
+    }
+
+]
---
+++ heheh/script.js
@@ -0,0 +1,18 @@
+//dados 
+
+let actualQuestion = 0 ; 
+
+
+function getActualqueston(){
+
+    let q = questions[actualQuestion] ;
+
+    console.log(q)
+
+}
+
+
+//events 
+
+
+//functions
