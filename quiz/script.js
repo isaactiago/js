cb908111187b9e45ff3483470b88c040e091b12c
isaactiago <141565649+isaactiago@users.noreply.github.com>
2024-04-20 12:23:19 -0300

    atualizeio pasta

--- quiz/script.js
+++ quiz/script.js
@@ -3,12 +3,14 @@
 
 let actualQuestion = 0 ; 
 
-let correctQuestion = 0 ; 
+let correcanswer = 0 ; 
 
 getActualqueston() ;
 
 
+//eventos 
 
+document.querySelector("button").addEventListener("click", reset)
 
 
 //funcoes
@@ -16,6 +18,9 @@ getActualqueston() ;
 function getActualqueston(){
 
     let q = questions[actualQuestion] ;
+
+    let pct = Math.floor((actualQuestion / questions.length) * 100)
+    document.querySelector(".progress--bar").style.width = `${pct}%`
 
     if(questions[actualQuestion]){
 
@@ -33,14 +38,16 @@ function getActualqueston(){
         document.querySelector('.options').innerHTML = getoptions ; 
     
         document.querySelectorAll('.options .option').forEach(item=> {
-            item.addEventListener('click', clickedOption)
+            item.addEventListener('click', clickedOptionEvent)
         })
   
         
     
 
     }else{
-        //
+        //acabou 
+
+        finish()
     }
     
  
@@ -54,7 +61,7 @@ function getActualqueston(){
 
 }
 
-function clickedOption(e){
+function clickedOptionEvent(e){
 
     let clickedOption = parseInt(e.target.getAttribute('data-op')) // revisa o porque dessa parte. assistir novamente o video para entender 
      
@@ -63,12 +70,72 @@ function clickedOption(e){
 
     //agora eu consigo fazer a verificação da resposta corret 
 
-    if(correctQuestion === clickedOption){
-        alert("kdslfk")
+    if(questions[actualQuestion].answer === clickedOption){
+        correcanswer++ ; // vai dizaer quantas eu acertei no final 
+      
+       
+       
+       
+        
     }
+  
+    actualQuestion++ ; //rever essa parte 
+    getActualqueston() ;
+
+ 
         
 
     
+}
+
+
+function finish(){
+
+    document.querySelector('.questionArea').style.display = 'none' ;
+    document.querySelector(".scoreArea").style.display = 'block'
+    document.querySelector(".progress--bar").style.width = `${100}%`
+
+    let scorePct = Math.floor((correcanswer/ questions.length) * 100)
+    document.querySelector('.scorePct').innerHTML = `${scorePct}%`
+    document.querySelector(".scoreText2").innerHTML = `Você respondeu ${questions.length} questões e acertou ${correcanswer} !!!!! `
+
+
+    if(correcanswer >= 3){
+
+        document.querySelector('.scoreText1').innerHTML = `Parabens !!!`
+        document.querySelector('.scoreText1').style.color = `green`
+
+        document.querySelector("scoreText2").innerHTML = `Acertou ${correcanswer} de ${questions.length}`
+
+    }else{
+        document.querySelector('.scoreText1').innerHTML = `Ruim, estude mais emm !!!`
+        document.querySelector('.scoreText1').style.color = `red`
+
+      
+
+    }
+
+   
+
+
+
+   
+}
+
+function reset(){
+
+  
+    correcanswer = 0 ;
+    actualQuestion = 0 ; 
+
+    document.querySelector(".scoreArea").style.display = 'none'
+
+    
+
+    getActualqueston() ;
+
+
+
 }
 
 
