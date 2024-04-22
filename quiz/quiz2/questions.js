aa717a841eb4218f11cccc595d6ebf1fc1a22ada
isaactiagowandermurem <141565649+isaactiago@users.noreply.github.com>
2024-04-22 17:25:20 -0300

    currented folders

---
+++ quiz/quiz2/questions.js
@@ -0,0 +1,81 @@
+let questions = [
+
+    {
+
+         question : 'Qual dos seguintes é um exemplo de um indicador financeiro chave para medir a saúde financeira de uma empresa?' ,
+
+         options: [
+
+             'Taxa de cliques (CTR)' ,
+             ' Margem de lucro líquido' ,
+             'Número de seguidores nas redes sociais',
+            'Tempo médio de permanência no site',
+
+         ] ,
+    } ,
+
+    {
+
+         answer : 1 , 
+
+         question : 'O que o acrônimo CRM representa no contexto empresarial?' ,
+
+         options : [
+
+            ' Customer Relationship Management',
+            ' Customer Retention Model',
+            'Customer Response Method',
+            'Customer Review Management',
+         ] ,
+         answer : 0 ,
+    
+    } ,
+
+
+    {
+
+         question : 'Qual é o principal objetivo de uma análise de mercado em um plano de negócios?' ,
+
+         options : [
+
+            ' Determinar a hierarquia organizacional da empresa',
+            'Avaliar as tendências do mercado e identificar oportunidades e ameaças',
+            'Calcular o retorno sobre investimento (ROI)',
+            'Estabelecer metas de vendas para o próximo trimestre',
+         ] ,
+
+         answer : 1 ,
+    } ,
+
+    {
+
+
+
+         question : 'Em que fase do ciclo de vida do produto uma estratégia de penetração de mercado é geralmente empregada?' ,
+
+         options : [
+
+            'Introdução',
+            'Crescimento',
+            'Maturidade',
+            'Declinio',
+         ] ,
+
+         answer : 0 , 
+    }
+
+    {
+
+         question :'O que significa a sigla ERP (Enterprise Resource Planning) em gestão empresarial?' , 
+
+         options : [
+        
+            'Electronic Record Processing' ,
+            'Efficient Resource Provision',
+            'Enterprise Resource Processing',
+            'Enterprise Resource Planning' ,
+        ] ,
+        answer : 3 ,
+
+    }
+]
---
+++ quiz/quiz2/script.js
@@ -0,0 +1,22 @@
+//Dados
+let currentQeustions  = questions ; //variavel que vai armazena a questao atual 
+
+//eventes 
+
+
+
+//functions 
+
+
+// Agora eu precisso fazer aparecer as questoex dinamicamente 
+
+
+function  ShowQeustions(){
+
+    let q = currentQeustions ;
+
+    console.log(q)
+
+
+
+}
