4460bcb5cb7ff204a1ea2369108578e53aaa31a7
isaactiago <141565649+isaactiago@users.noreply.github.com>
2024-04-25 09:25:03 -0300

    updaste

--- pratica/script.js
+++ pratica/script.js
@@ -1,33 +1,3 @@
-/* 
-
-function clicar(){
-    alert("sdjck")
-
-    const h1 = document.querySelector("h1") ; 
-
-    h1.style.color = "green" ;
-}
-
-let botao = document.querySelector(".buttom") ;
-
-botao.addEventListener("click" , function (){
-    
-    botao.innerHTML = "oi"
-    clicar()
-    
-
-})
-
-
-let h2 = document.querySelector(".topico"); 
-
-h2.innerHTML += "<br> outro ai "
-
-
-*/
-
-
- 
 
 
 
