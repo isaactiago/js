0326ccf2c1fbac7edf550b2f2e36a92a33f95e5a
isaactiago <141565649+isaactiago@users.noreply.github.com>
2024-04-27 14:41:27 -0300

    upfate

--- pratica/script.js
+++ pratica/script.js
@@ -1,7 +1,11 @@
 //dados 
 
-
+let cart = [] //variaval que vai armazena o array 
 let modalQt = 1 ; //variavel que armazenara a quantidade de pizza que tu vai querer 
+
+let modalKey = 0 ; // //Variavel qeu vai armazena o key, para eu saber qual pizza que é na hora do que for por carrinho 
+
+
 
 const c = (el) =>{
     return document.querySelector(el)
@@ -40,6 +44,8 @@ pizzaJson.map((item,index)=>{
 
         e.preventDefault() ;
 
+      
+
         //agora é abrir o quadro 
         document.querySelector('.pizzaWindowArea').style.opacity = 0 ;
         document.querySelector('.pizzaWindowArea').style.display = 'flex' ; 
@@ -59,6 +65,29 @@ pizzaJson.map((item,index)=>{
         c(".pizzaInfo--actualPrice").innerHTML = `R$${pizzaJson[key].price.toFixed(2)}`
 
 
+        //troca de tamanho das pizzas e informa o size 
+        c(".pizzaInfo--size.selected").classList.remove('selected')
+        cs('.pizzaInfo--size').forEach( (size,sizeIndex)=>{
+           
+            if(sizeIndex === 2){
+                size.classList.add('selected')
+            }
+
+            size.querySelector("span").innerHTML = pizzaJson[key].sizes[sizeIndex]
+        })
+
+
+        
+        //vai resetar a quantidade quando fechar o modal 
+        modalQt = 1 ; 
+        c('.pizzaInfo--qt').innerHTML = modalQt
+
+
+
+        //Variavel qeu vai armazena o key, para eu saber qual pizza que é na hora do que for por carrinho 
+        modalKey = key ; 
+
+
         
     
 
@@ -75,7 +104,7 @@ pizzaJson.map((item,index)=>{
    
 })
 
-// evento dos modals
+// evento dos modals -------------------------------
 
 //fecha o qadrado 
 
@@ -92,6 +121,167 @@ function closeModal(){
 cs(".pizzaInfo--cancelButton , .pizzaInfo--cancelMobileButton").forEach((item) => {
      item.addEventListener("click", closeModal)
 });
+
+
+
+// evento de quanto clica no mais e menos, adiciona ou dimiunuir a quantidade de pizza
+
+document.querySelector('.pizzaInfo--qtmais').addEventListener('click', ()=>{
+
+    modalQt++ ;
+    c('.pizzaInfo--qt').innerHTML = modalQt
+    
+})
+
+
+document.querySelector('.pizzaInfo--qtmenos').addEventListener('click', ()=>{
+
+   if(modalQt > 1){
+
+    modalQt-- ;
+    c('.pizzaInfo--qt').innerHTML = modalQt
+    
+   }
+
+})
+
+
+//troca de tamanho das pizzas e informa o size 
+cs('.pizzaInfo--size').forEach( (size,sizeIndex)=>{
+
+    size.addEventListener("click",()=>{
+        c(".pizzaInfo--size.selected").classList.remove("selected");
+        size.classList.add('selected');
+        
+
+    })
+
+})
+
+
+//eventos do carrinho -----------------------------------------------
+
+
+c('.pizzaInfo--addButton').addEventListener("click", ()=>{
+
+     // 3 informações obrigatorias para por no carrinho 
+
+     //qual pizza ? = modalKey
+
+     //qual o tamanho ? 
+
+        let size = parseInt(c(".pizzaInfo--size.selected").getAttribute("data-key"))
+        console.log(size)
+
+     //Quantas pizzas  = modalQt
+
+
+     //adiciona ao meu carrinho/array 
+
+     //tenho que criar um identificador 
+
+     let identificador =  pizzaJson[modalKey].id+"@"+size
+
+     //verificar se eu ja tenho aquele item adicionado ao carrinho ; 
+     let key = cart.findIndex((item)=>{
+        return item.identificador == identificador;
+     }) ;
+
+     if(key > -1){
+
+        //se eu achar o item no carrinho eu, so aumento a quantidade ;
+        cart[key].qt += modalQt ; 
+
+     }else{
+
+        //se eu nao achar o item no carrinho, eu adiciono 
+        cart.push({
+            identificador,
+            id : pizzaJson[modalKey].id ,
+            size,
+            qt : modalQt ,
+         })
+    
+
+     }
+
+     
+
+
+
+
+
+     
+    closeModal() ; 
+    updateCart();
+    
+})
+
+
+function updateCart(){
+
+    //verirfica se tenho itens no carrinho 
+    //se tiver eu abro o carrinho
+    if(cart.length > 0){
+       c("aside").classList.add("show") ;
+
+       //limpa o carrinho 
+       c('.cart').innerHTML = '' ;
+
+       let subtotal = 0 ;
+       let desconto = 0 ; 
+       let total = 0 ; 
+
+       //agora temos que fazer um looping no carrinho pois, eu vou peagr item a item, pra exibir na tela 
+
+       for(let i in cart){
+            //primeiro, é identificar qual é a pizza 
+             //usamos o find para procurran no meu pizzaJson, e la dentro eu pego o id da pizza 
+            let pizzaItem = pizzaJson.find((item)=>{
+                 return item.id == cart[i].id
+             })
+
+              //clona o cart--item 
+
+            let cartItem = c(".cart--item").cloneNode(true) ; 
+
+             document.querySelector(".cart").append(cartItem) ; 
+
+            let pizzaSizeName ;
+
+            switch(cart[i].size){
+                 case 0 : 
+                    pizzaSizeName = 'P'
+                 break ;
+                 case 1 : 
+                      pizzaSizeName = 'M'
+                 break;
+                case 2 : 
+                      pizzaSizeName = 'G'
+                break ;
+            }
+
+            let pizzaname = `${pizzaItem.name} ${pizzaSizeName}`
+
+            cartItem.querySelector('.cart--item img').src = pizzaItem.img ; 
+            cartItem.querySelector(".cart--item-nome").innerHTML = pizzaname ; 
+            cartItem.querySelector('.cart--item--qt').innerHTML = modalQt ; 
+
+
+
+            
+        }
+
+       c(".cart--item").cloneNode(true) ;
+    }else{
+
+        c("aside").classList.remove("show")
+
+    }
+
+}
+
+
 
   
 
