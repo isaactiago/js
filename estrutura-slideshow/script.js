b961ccab2348d1886d2593f4fd7f2edcead7d702
isaactiagowandermurem <141565649+isaactiago@users.noreply.github.com>
2024-04-30 09:55:48 -0300

    update js

--- estrutura-slideshow/script.js
+++ estrutura-slideshow/script.js
@@ -6,7 +6,7 @@ let totalSliders = document.querySelectorAll(".slider--item").length ;
 document.querySelector(".slider--width").style.width = `calc(100vw * ${totalSliders})`; // A largura vai ser a largura da tela vezes a quantidade de sliders
 
 document.querySelector('.slider--controls').style.height =
-`${document.querySelector('.slider').clientHeight}px` ;  /* Vai pegar a altura do meu slider e joga aqui */
+`${document.querySelector(".slider").clientHeight}px` ;  /* Vai pegar a altura do meu slider e joga aqui */
 // clientHeight vai pegar a altura de uma div que ja tem altura 
 
 
